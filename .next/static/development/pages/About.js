ba1a4f15e9ad9e70d35221fb480012108fc71da4
Daniel <daniel.won.choi@gmail.com>
2019-04-11 18:08:44 -0500

    using next to create git bootcamp website

---
+++ .next/static/development/pages/About.js
@@ -0,0 +1,93 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/About.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2FAbout&absolutePagePath=%2FUsers%2Fdanielchoi%2FDesktop%2FHack4Impact%2Fh4i-cs-git-bootcamp%2Fpages%2FAbout.js!./":
+/*!********************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2FAbout&absolutePagePath=%2FUsers%2Fdanielchoi%2FDesktop%2FHack4Impact%2Fh4i-cs-git-bootcamp%2Fpages%2FAbout.js ***!
+  \********************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/About", function() {
+      var page = __webpack_require__(/*! ./pages/About.js */ "./pages/About.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/About.js */ "./pages/About.js", function() {
+          if(!next.router.components["/About"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/About.js */ "./pages/About.js")
+          next.router.update("/About", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_7aff549c98b978433226 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_7aff549c98b978433226 */ "dll-reference dll_7aff549c98b978433226"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/About.js":
+/*!************************!*\
+  !*** ./pages/About.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return About; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/danielchoi/Desktop/Hack4Impact/h4i-cs-git-bootcamp/pages/About.js";
+
+function About() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "This is the about page"));
+}
+
+/***/ }),
+
+/***/ 0:
+/*!************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2FAbout&absolutePagePath=%2FUsers%2Fdanielchoi%2FDesktop%2FHack4Impact%2Fh4i-cs-git-bootcamp%2Fpages%2FAbout.js ***!
+  \************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2FAbout&absolutePagePath=%2FUsers%2Fdanielchoi%2FDesktop%2FHack4Impact%2Fh4i-cs-git-bootcamp%2Fpages%2FAbout.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2FAbout&absolutePagePath=%2FUsers%2Fdanielchoi%2FDesktop%2FHack4Impact%2Fh4i-cs-git-bootcamp%2Fpages%2FAbout.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_7aff549c98b978433226":
+/*!*******************************************!*\
+  !*** external "dll_7aff549c98b978433226" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_7aff549c98b978433226;
+
+/***/ })
+
+},[[0,"static/runtime/webpack.js"]]]));;
+//# sourceMappingURL=About.js.map
